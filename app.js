89ffcc45cbfe3a340498e3b705342a720f5039f1
davillboy <71083807+davillboy@users.noreply.github.com>
2024-04-22 16:47:20 +0700

    first change

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
 // write code here
+<script>
+        let userInput;
+        let userObject = {};
+
+        while (true) {
+        userInput = prompt("Enter key-value pair (key:value), or type 'stop' to finish:");
+
+        if (userInput.toLowerCase() === "stop") {
+            break;
+        }
+
+        let [key, value] = userInput.split(':');
+
+        key = key.trim();
+        value = value.trim();
+
+        userObject[key] = value;
+        }
+
+        console.log(userObject);
+
+    </script>
